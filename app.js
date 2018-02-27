cf26bea4e9a8165ac18c9608d3e30ab1ecfcf65f
Balazska <fodorbazsa@gmail.com>
2018-02-27 09:43:16 +0100

    include bootstrap and create login and index pages

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-var express = require("express");
-
+var express = require("express"); 
 //create app
 var app = express();
+
+//include jquery and bootstrap
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+
 //serving static sites
 app.use(express.static('public'));
 
