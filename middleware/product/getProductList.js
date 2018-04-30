72e74241b83519520b9e59befbdb7ecb1bdceeea
Balazska <fodorbazsa@gmail.com>
2018-04-30 17:36:16 +0200

    add category column to admin page

--- middleware/product/getProductList.js
+++ middleware/product/getProductList.js
@@ -10,12 +10,15 @@ module.exports = function (objectrepository) {
     return function (req, res, next) {
         var Product = objectrepository.productModel;
 
-        Product.find(function(err,products){
-            res.locals.products = products;
-            console.log("get all products");
-            return next();
-        });
-        
+        Product.find().populate('type').exec(function(err,products){
+            if(err){
+                console.log(err);
+            } else{
+                res.locals.products = products;
+                console.log("get all products");
+                return next();
+            }
+        });    
     };
 
 };
